433f3f52acd42c61cd52a868c0f84ed01b5a08a8
mjounes2 <113125062+mjounes2@users.noreply.github.com>
2024-01-22 14:25:17 +0400

    update portainer

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# Install Docker
+
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y docker.io
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# install Docker 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 
 # Create Docker volume for Portainer data
 docker volume create portainer_data
+
+# Copy local folder to Docker volume location
+sudo cp -r ./portainer_data /var/lib/docker/volumes
 
 # Run Portainer container
 docker run -d -p 9443:9443 --name=portainer --restart=always \
@@ -15,3 +34,4 @@ docker run -d -p 9443:9443 --name=portainer --restart=always \
   --logo "https://raw.githubusercontent.com/mjounes2/SOC/SEIM/VVMlogo.png"
 
 echo "Portainer is now running on port 9443."
+
